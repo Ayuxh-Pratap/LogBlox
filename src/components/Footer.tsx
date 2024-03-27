a235da0eeb9f2a27f155242acd6e6d24934e2df9
Ayush Pratap Singh <161467009+Ayuxh-Pratap@users.noreply.github.com>
2024-03-27 16:53:37 +0530

    layout fix v2

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,6 @@
-import Image from "next/image";
-import Logo from "../../public/assets/Logo.svg";
-import Facebook from "../../public/assets/Facebook.svg";
-import Twitter from "../../public/assets/X.svg";
-import Feed from "../../public/assets/Feed.svg";
-import { IoIosMailUnread } from "react-icons/io";
-import { FaWhatsapp } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa";
-import { FaGithub } from "react-icons/fa";
+import { SiGmail } from "react-icons/si";
+import { FaLinkedin } from "react-icons/fa";
+import { IoLogoWhatsapp } from "react-icons/io";
 
 export function Footer() {
 
@@ -36,9 +30,9 @@ export function Footer() {
       </p>
 
       <div className='flex items-center justify-center gap-6 pt-[56px] sm:pt-5 text-gray-700'>
-        <a href='mailto:ayuaharshita14@gmail.com' className='text-2xl'><IoIosMailUnread /></a>
-        <a href='https://github.com/Ayuxh-Pratap' className='text-2xl'><FaGithub /></a>
-        <a href='https://wa.me/9460865839' className='text-2xl'><FaWhatsapp /></a>
+        <a href='mailto:ayuaharshita14@gmail.com' className='text-2xl'><SiGmail /></a>
+        <a href='https://wa.me/9460865839' className='text-2xl'><IoLogoWhatsapp /></a>
+        <a href="https://www.linkedin.com/in/ayush-pratap-singh-7716b2295/" className="text-2xl" target='_blank'><FaLinkedin /></a>
       </div>
     </div>
   );
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { Button } from './ui/button'
 import { SiQuicktime } from "react-icons/si";
 import { FaGithub } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa";
 
 export default function NavBar() {
   return (
@@ -12,10 +11,7 @@ export default function NavBar() {
         <h1 className='font-bold text-xl'>LogBlox</h1>
       </div>
       <div className='flex items-center gap-5'>
-        <div className='flex items-center gap-3'>
           <a href="https://github.com/Ayuxh-Pratap" target='_blank'><FaGithub className='text-2xl' /></a>
-          <a href="https://www.linkedin.com/in/ayush-pratap-singh-7716b2295/" target='_blank'><FaLinkedin className='text-2xl' /></a>
-        </div>
         <Button>Logout</Button>
       </div>
     </div>
